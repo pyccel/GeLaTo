1e41747eebfb20431dd2fde297f20a1642ef810b
ratnania <ratnaniahmed@gmail.com>
2018-05-08 16:11:52 +0200

    add new examples glt 2d and 3d for constants and functions

--- tests/glt/test_2d.py
+++ tests/glt/test_2d.py
@@ -10,6 +10,7 @@ from sympy.core.containers import Tuple
 from sympy import symbols
 from sympy import Symbol
 from sympy import Lambda
+from sympy import Function
 from sympy import IndexedBase
 
 from gelato.glt import glt_symbol
@@ -70,6 +71,7 @@ def test_2d_1():
 # ...
 
 # ...
+# TODO
 def test_2d_2():
     x,y = symbols('x y')
 
@@ -92,6 +94,7 @@ def test_2d_2():
 # ...
 
 # ...
+# TODO
 def test_2d_3():
     x,y = symbols('x y')
 
@@ -145,11 +148,13 @@ def test_2d_4():
     u = Symbol('u')
     v = Symbol('v')
 
+    c = Constant('c')
+
     b0 = Constant('b0')
     b1 = Constant('b1')
     b = Tuple(b0, b1)
 
-    a = Lambda((x,y,v,u), 0.2 * u * v + Dot(b, Grad(v)) * u + Dot(b, Grad(u)) * v)
+    a = Lambda((x,y,v,u), c * u * v + Dot(b, Grad(v)) * u + Dot(b, Grad(u)) * v)
     print('> input       := {0}'.format(a))
 
     # ...  create a finite element space
@@ -175,7 +180,73 @@ def test_2d_4():
 
     # ...
     symbol_f90 = compile_symbol('symbol_4', a, V,
-                                d_constants={'b0': 0.1, 'b1': 1.},
+                                d_constants={'b0': 0.1, 'b1': 1., 'c': 0.2},
+                                backend='fortran')
+    # ...
+
+    # ... example of symbol evaluation
+    t1 = linspace(-pi,pi, ne1+1)
+    t2 = linspace(-pi,pi, ne2+1)
+    x1 = linspace(0.,1., ne1+1)
+    x2 = linspace(0.,1., ne2+1)
+    e = zeros((ne1+1, ne2+1), order='F')
+    symbol_f90(x1,x2,t1,t2, e)
+    # ...
+
+    print('')
+# ...
+
+# ...
+def test_2d_5():
+    x,y = symbols('x y')
+
+    u = Symbol('u')
+    v = Symbol('v')
+
+    b = Function('b')
+
+    a = Lambda((x,y,v,u), Dot(Grad(u), Grad(v)) + b(x,y)*u*v)
+    print('> input       := {0}'.format(a))
+
+    # ...  create a finite element space
+    p1  = 2 ; p2  = 2
+    ne1 = 8 ; ne2 = 8
+
+    print('> Grid   :: [{ne1},{ne2}]'.format(ne1=ne1, ne2=ne2))
+    print('> Degree :: [{p1},{p2}]'.format(p1=p1, p2=p2))
+
+    grid_1 = linspace(0., 1., ne1+1)
+    grid_2 = linspace(0., 1., ne2+1)
+
+    V1 = SplineSpace(p1, grid=grid_1)
+    V2 = SplineSpace(p2, grid=grid_2)
+
+    V = TensorSpace(V1, V2)
+    # ...
+
+    # ... create a glt symbol from a string without evaluation
+    expr = glt_symbol(a, space=V)
+    print('> glt symbol  := {0}'.format(expr))
+    # ...
+
+    # ... user defined function
+    def b(x,y):
+        r = 1.+ x*y
+        return r
+    # ...
+
+    # ... create an interactive pyccel context
+    from pyccel.epyccel import ContextPyccel
+
+    context = ContextPyccel(name='context_5')
+    context.insert_function(b, ['double', 'double'], kind='function', results=['double'])
+
+    context.compile()
+    # ...
+
+    # ...
+    symbol_f90 = compile_symbol('symbol_5', a, V,
+                                context=context,
                                 backend='fortran')
     # ...
 
@@ -198,3 +269,4 @@ if __name__ == '__main__':
 #    test_2d_2()
 #    test_2d_3()
     test_2d_4()
+    test_2d_5()
--- tests/glt/test_3d.py
+++ tests/glt/test_3d.py
@@ -8,6 +8,7 @@ from sympy.core.containers import Tuple
 from sympy import symbols
 from sympy import Symbol
 from sympy import Lambda
+from sympy import Function
 from sympy import IndexedBase
 
 from gelato.glt import glt_symbol
@@ -116,6 +117,140 @@ def test_3d_3():
 # ...
 
 # ...
+def test_3d_4():
+    x,y,z = symbols('x y z')
+
+    u = Symbol('u')
+    v = Symbol('v')
+
+    c = Constant('c')
+
+    b0 = Constant('b0')
+    b1 = Constant('b1')
+    b2 = Constant('b2')
+    b = Tuple(b0, b1, b2)
+
+    a = Lambda((x,y,z,v,u), c * u * v + Dot(b, Grad(v)) * u + Dot(b, Grad(u)) * v)
+    print('> input       := {0}'.format(a))
+
+    # ...  create a finite element space
+    p1  = 2 ; p2  = 2 ; p3  = 2
+    ne1 = 2 ; ne2 = 2 ; ne3 = 2
+    # ...
+
+    print('> Grid   :: [{},{},{}]'.format(ne1, ne2, ne3))
+    print('> Degree :: [{},{},{}]'.format(p1, p2, p3))
+
+    grid_1 = linspace(0., 1., ne1+1)
+    grid_2 = linspace(0., 1., ne2+1)
+    grid_3 = linspace(0., 1., ne3+1)
+
+    V1 = SplineSpace(p1, grid=grid_1)
+    V2 = SplineSpace(p2, grid=grid_2)
+    V3 = SplineSpace(p3, grid=grid_3)
+
+    V = TensorSpace(V1, V2, V3)
+    # ...
+
+    # ... create a glt symbol from a string without evaluation
+    expr = glt_symbol(a, space=V)
+    print('> glt symbol  := {0}'.format(expr))
+    # ...
+
+    # ...
+    symbol_f90 = compile_symbol('symbol_4', a, V,
+                                d_constants={'b0': 0.1, 'b1': 1., 'b2': 1., 'c': 0.2},
+                                backend='fortran')
+    # ...
+
+    # ... example of symbol evaluation
+    t1 = linspace(-pi,pi, ne1+1)
+    t2 = linspace(-pi,pi, ne2+1)
+    t3 = linspace(-pi,pi, ne3+1)
+    x1 = linspace(0.,1., ne1+1)
+    x2 = linspace(0.,1., ne2+1)
+    x3 = linspace(0.,1., ne3+1)
+    e = zeros((ne1+1, ne2+1, ne3+1), order='F')
+    symbol_f90(x1,x2,x3,t1,t2,t3, e)
+    # ...
+
+    print('')
+# ...
+
+# ...
+def test_3d_5():
+    x,y,z = symbols('x y z')
+
+    u = Symbol('u')
+    v = Symbol('v')
+
+    b = Function('b')
+
+    a = Lambda((x,y,z,v,u), Dot(Grad(u), Grad(v)) + b(x,y,z)*u*v)
+    print('> input       := {0}'.format(a))
+
+    # ...  create a finite element space
+    p1  = 2 ; p2  = 2 ; p3  = 2
+    ne1 = 2 ; ne2 = 2 ; ne3 = 2
+    # ...
+
+    print('> Grid   :: [{},{},{}]'.format(ne1, ne2, ne3))
+    print('> Degree :: [{},{},{}]'.format(p1, p2, p3))
+
+    grid_1 = linspace(0., 1., ne1+1)
+    grid_2 = linspace(0., 1., ne2+1)
+    grid_3 = linspace(0., 1., ne3+1)
+
+    V1 = SplineSpace(p1, grid=grid_1)
+    V2 = SplineSpace(p2, grid=grid_2)
+    V3 = SplineSpace(p3, grid=grid_3)
+
+    V = TensorSpace(V1, V2, V3)
+    # ...
+
+    # ... create a glt symbol from a string without evaluation
+    expr = glt_symbol(a, space=V)
+    print('> glt symbol  := {0}'.format(expr))
+    # ...
+
+    # ... user defined function
+    def b(x,y,z):
+        r = 1.+ x*y*z
+        return r
+    # ...
+
+    # ... create an interactive pyccel context
+    from pyccel.epyccel import ContextPyccel
+
+    context = ContextPyccel(name='context_5')
+    context.insert_function(b, ['double', 'double', 'double'], kind='function', results=['double'])
+
+    context.compile()
+    # ...
+
+    # ...
+    symbol_f90 = compile_symbol('symbol_5', a, V,
+                                context=context,
+                                backend='fortran')
+    # ...
+
+    # ... example of symbol evaluation
+    t1 = linspace(-pi,pi, ne1+1)
+    t2 = linspace(-pi,pi, ne2+1)
+    t3 = linspace(-pi,pi, ne3+1)
+    x1 = linspace(0.,1., ne1+1)
+    x2 = linspace(0.,1., ne2+1)
+    x3 = linspace(0.,1., ne3+1)
+    e = zeros((ne1+1, ne2+1, ne3+1), order='F')
+    symbol_f90(x1,x2,x3,t1,t2,t3, e)
+    # ...
+
+    print('')
+# ...
+
+
+
+# ...
 def test_3d_4a():
     x,y,z = symbols('x y z')
 
@@ -176,3 +311,5 @@ if __name__ == '__main__':
 #    test_3d_3()
 #    test_3d_4a()
 #    test_3d_4b()
+    test_3d_4()
+    test_3d_5()
