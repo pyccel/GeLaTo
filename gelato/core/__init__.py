2a0f847ba49b197e2249443b9689a21289d59f45
ratnania <ratnaniahmed@gmail.com>
2018-07-25 08:14:46 +0200

    move code to symfe

--- gelato/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .algebra      import *
-from .basic        import *
-from .derivatives  import *
-from .generic      import *
-from .space        import *
-from .expr         import *
--- gelato/fem/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-from .assembly      import *
-from .bilinear_form import *
-from .linear_form   import *
-from .function_form import *
-from .field         import *
-from .symbol        import *
