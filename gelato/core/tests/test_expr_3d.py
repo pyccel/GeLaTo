7c3e3b386058665d2ff42cdd6a9336016857e5bb
ratnania <ratnaniahmed@gmail.com>
2018-07-25 11:51:56 +0200

    add 3d tests

--- gelato/core/tests/test_expr_3d.py
+++ gelato/core/tests/test_expr_3d.py
@@ -17,71 +17,77 @@ from symfe.core import BilinearForm
 
 from gelato.core import gelatize
 
-
 # ...
-def test_gelatize_2d_1():
-    print('============ test_gelatize_2d_1 =============')
+def test_gelatize_3d_1():
+    print('============ test_gelatize_3d_1 =============')
 
-    V = H1Space('V', ldim=2)
+    V = H1Space('V', ldim=3)
     V_0 = H1Space('V_0', ldim=1, coordinates=['x'])
     V_1 = H1Space('V_1', ldim=1, coordinates=['y'])
+    V_2 = H1Space('V_2', ldim=1, coordinates=['z'])
 
     v = TestFunction(V, name='v')
     u = TestFunction(V, name='u')
 
-    nx, ny = symbols('nx ny', integer=True)
+    nx, ny, nz = symbols('nx ny nz', integer=True)
 
     m_0, s_0, a_0 = symbols('m_0 s_0 a_0', real=True)
     m_1, s_1, a_1 = symbols('m_1 s_1 a_1', real=True)
+    m_2, s_2, a_2 = symbols('m_2 s_2 a_2', real=True)
 
     c = Constant('c')
 
     bx = Constant('bx')
     by = Constant('by')
-    b = Tuple(bx, by)
+    bz = Constant('bz')
+    b = Tuple(bx, by, bz)
 
     # ...
-    expected = m_0*m_1/(nx*ny)
+    expected = m_0*m_1*m_2/(nx*ny*nz)
     assert(gelatize(BilinearForm((v,u), u*v), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = m_1*nx*s_0/ny
+    expected = m_1*m_2*nx*s_0/(ny*nz)
     assert(gelatize(BilinearForm((v,u), dx(u)*dx(v)), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = a_1*m_0/nx
+    expected = a_1*m_0*m_2/(nx*nz)
     assert(gelatize(BilinearForm((v,u), dy(u) * v), evaluate=False) == expected)
     # ...
 
     # ...
-    expected =  a_0*m_1/ny
+    expected = a_0*m_1*m_2/(ny*nz)
     assert(gelatize(BilinearForm((v,u), dx(u) * v), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = m_0*ny*s_1/nx + m_1*nx*s_0/ny
+    expected = m_0*m_1*nz*s_2/(nx*ny) + m_0*m_2*ny*s_1/(nx*nz) + m_1*m_2*nx*s_0/(ny*nz)
     assert(gelatize(BilinearForm((v,u), dot(grad(v), grad(u))), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = a_0*m_1/ny + a_1*m_0/nx + m_0*ny*s_1/nx + m_1*nx*s_0/ny
+    expected = (a_0*m_1*m_2/(ny*nz) + a_1*m_0*m_2/(nx*nz) +
+                m_0*m_1*nz*s_2/(nx*ny) + m_0*m_2*ny*s_1/(nx*nz) +
+                m_1*m_2*nx*s_0/(ny*nz))
     assert(gelatize(BilinearForm((v,u), dot(grad(v), grad(u)) + dx(u)*v + dy(u)*v), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = -bx*a_0*m_1/ny - by*a_1*m_0/nx
+    expected = -bx*a_0*m_1*m_2/(ny*nz) - by*a_1*m_0*m_2/(nx*nz) - bz*a_2*m_0*m_1/(nx*ny)
     assert(gelatize(BilinearForm((v,u), dot(b, grad(v)) * u), evaluate=False) == expected)
     # ...
 
     # ...
-    expected = bx**2*m_1*nx*s_0/ny - 2*bx*by*a_0*a_1 + by**2*m_0*ny*s_1/nx
+    expected = (bx**2*m_1*m_2*nx*s_0/(ny*nz) - 2*bx*by*a_0*a_1*m_2/nz -
+                2*bx*bz*a_0*a_2*m_1/ny + by**2*m_0*m_2*ny*s_1/(nx*nz) -
+                2*by*bz*a_1*a_2*m_0/nx + bz**2*m_0*m_1*nz*s_2/(nx*ny))
     assert(gelatize(BilinearForm((v,u), dot(b, grad(v)) * dot(b, grad(u))), evaluate=False) == expected)
     # ...
 
     degrees = None
-#    degrees = [2, 1]
+#    degrees = [2, 1, 1]
 
 #    evaluate = True
     evaluate = False
@@ -96,4 +102,5 @@ def test_gelatize_2d_1():
 
 # .....................................................
 if __name__ == '__main__':
-    test_gelatize_2d_1()
+
+    test_gelatize_3d_1()
