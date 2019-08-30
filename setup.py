458911ebfbc8fe74807279223ecb436ff8ff80b4
Yaman Güçlü <yaman.guclu@gmail.com>
2019-08-30 18:31:19 +0200

    Clean up installation procedure with pip (#4)
    * Move dependencies list from 'requirements.txt' to 'setup.py'.
    
    * Move library version to separate file.
    
    NOTE: gelato now provides version to user:
    
    In [1]: import gelato; gelato.__version__
    Out[1]: '0.9.3'
    
    * Enable tests with Python-3.7.
    
    * Update __version__

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # -*- coding: UTF-8 -*-
 #! /usr/bin/python
 
-import sys
+from pathlib import Path
 from setuptools import setup, find_packages
 
+# ...
+# Read library version into '__version__' variable
+path = Path(__file__).parent / 'gelato' / 'version.py'
+exec(path.read_text())
+# ...
+
 NAME    = 'gelato'
-VERSION = '0.9.3'
+VERSION = __version__
 AUTHOR  = 'Ahmed Ratnani'
 EMAIL   = 'ahmed.ratnani@ipp.mpg.de'
 URL     = 'https://github.com/ratnania/GeLaTo'
@@ -26,17 +32,28 @@ setup_args = dict(
 #    download_url     = URL+'/tarball/master',
 )
 
+install_requires = [
+
+    # Third-party libraries from PyPi
+    'sympy>=1.2',
+    'numpy>=1.13',
+    'scipy>=0.18',
+    'matplotlib',
+
+    # Our libraries from PyPi
+    'sympde',
+]
+
 # ...
 packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 # ...
 
 def setup_package():
-    if 'setuptools' in sys.modules:
-        setup_args['install_requires'] = ['numpy']
 
-    setup(packages = packages, \
-          include_package_data = True, \
-          zip_safe=True, \
+    setup(packages = packages,
+          include_package_data = True,
+          install_requires = install_requires,
+          zip_safe = True,
           **setup_args)
 
 
